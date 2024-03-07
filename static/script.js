5b00c416d31bb93c85e79075b815656a736cfd06
Mirko117 <suvajac117@gmail.com>
2024-03-07 12:07:49 +0100

    Optimization

--- static/script.js
+++ static/script.js
@@ -6,8 +6,8 @@ let boardHeight = 640;
 let context;
 
 //bird
-let birdWidth = 26; //width/height ratio = 408/228 = 17/12
-let birdHeight = 36;
+let birdWidth = 35;
+let birdHeight = 50;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
 let birdImg;
