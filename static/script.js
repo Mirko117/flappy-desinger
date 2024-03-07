2eb99d9869ee5ac83475cba69000e4906251676c
Mirko117 <suvajac117@gmail.com>
2024-03-07 00:39:28 -0800

    Texture changes

--- static/script.js
+++ static/script.js
@@ -6,8 +6,8 @@ let boardHeight = 640;
 let context;
 
 //bird
-let birdWidth = 34; //width/height ratio = 408/228 = 17/12
-let birdHeight = 24;
+let birdWidth = 26; //width/height ratio = 408/228 = 17/12
+let birdHeight = 36;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
 let birdImg;
